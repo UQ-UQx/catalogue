45666d6716c4cc4f1e09c2f990783f1ab776393f
Ankith Konda <a.konda@uq.edu.au>
2018-07-27 15:27:47 +1000

    Add component renders test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+export const Notification = (err) => {
+  return "Something went wrong."
+}
+
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      error:false
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +27,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.error ? <Notification /> : ''}
       </div>
     );
   }
