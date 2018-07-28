917f6cc6e077c13cee18f3386b0609d51b30f4a3
Ankith Konda <a.konda@uq.edu.au>
2018-07-28 17:39:40 +1000

    test lint in travis build

---
+++ src/components/TestComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+
+class TestComponent extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      error:true
+    }
+  }
+
+  render() {
+    return (
+      <div className="TestComponent">
+       
+      </div>
+    );
+  }
+}
+
+export default TestComponent;
